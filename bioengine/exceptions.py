29f38be1c11efd94aafa49564f6e0eb7882964d9
Martin Hjelmare <marhje52@kth.se>
2019-10-27 20:30:05 +0100

    Rename base exception

--- bioengine/exceptions.py
+++ bioengine/exceptions.py
@@ -1,11 +1,11 @@
 """Provide bioengine exceptions."""
 
 
-class IOEngineException(Exception):
+class BioEngineException(Exception):
     """Represent the base BioEngine exception."""
 
 
-class UnsupportedAPI(IOEngineException):
+class UnsupportedAPI(BioEngineException):
     """Represent an unsupported api exception."""
 
     def __init__(self):
